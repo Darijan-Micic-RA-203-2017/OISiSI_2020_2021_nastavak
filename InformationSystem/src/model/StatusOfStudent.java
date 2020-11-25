c5019b5fdf38c71137a377fc09f7a3076b08c1d0
Darijan_Micic_RA_203_2017 <darijan.micic10@gmail.com>
2020-11-25 22:55:13 +0100

    #student Dodavanje reference u nabrajanju StatusOfStudent
    Dodata je referenca na materijale sa nultih vežbi, odakle je preuzet kod.

--- InformationSystem/src/model/StatusOfStudent.java
+++ InformationSystem/src/model/StatusOfStudent.java
@@ -1,11 +1,11 @@
 package model;
 
 public enum StatusOfStudent {
+	/** REFERENCA: Materijali za vežbe (v0 -> VEZBE_03_Date-enum-kolekcije-sortiranje.pdf) */
 	B, S;
 	
 	private static final String[] description = {"budžet", "samofinansiranje"};
 	
-	@Override
 	public String toString() {
 		return StatusOfStudent.description[this.ordinal()];
 	}
