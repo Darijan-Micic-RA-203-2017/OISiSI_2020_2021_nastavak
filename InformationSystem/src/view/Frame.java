3ea51f33ab912c814b999565ee14e7dafd4adf5c
OgnjenSr <srdic.ra46.2018@uns.ac.rs>
2020-12-05 21:14:53 +0100

    glavni_prozor glavni prozor sa svim svojim komponentama

---
+++ InformationSystem/src/view/Frame.java
@@ -0,0 +1,49 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+import view.MenuBar;
+import view.ToolBar;
+
+public class Frame extends JFrame {
+	
+
+	private static final long serialVersionUID = 4L;
+	
+	public Frame() {
+		
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Dimension screenSize = kit.getScreenSize();
+		int screenHeight = screenSize.height;
+		int screenWidth = screenSize.width;
+		setSize(screenWidth * 3 / 4, screenHeight * 3 / 4);
+		setTitle("Studentska služba");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		this.add(panel);
+		
+		JLabel lbl = new JLabel("TODO: Prikaz entiteta sistema");
+		lbl.setToolTipText("Prikaz entiteta sistema ce biti naknadno uradjen");
+		panel.add(lbl);
+		
+		MenuBar menu = new MenuBar();
+		this.setJMenuBar(menu);
+		
+		ToolBar toolbar = new ToolBar();
+		add(toolbar, BorderLayout.NORTH);
+		
+		StatusBar statusbar = new StatusBar();
+		add(statusbar, BorderLayout.SOUTH);
+	}
+
+}
