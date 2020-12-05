0ffbc23df65d9a19d3cc7e7d884f055d6fdd949a
OgnjenSr <srdic.ra46.2018@uns.ac.rs>
2020-12-05 21:27:25 +0100

    #main dodavanje maina radi testiranja uspesnosti

---
+++ InformationSystem/src/application/App.java
@@ -0,0 +1,14 @@
+package application;
+
+import view.Frame;
+
+public class App {
+	
+	public static void main(String[] args) {
+
+		Frame frame1 = new Frame();
+		frame1.setVisible(true);
+		
+	}
+
+}
