444b5ecae7bfb632a6d0ea24c238622970290077
Darijan_Micic_RA_203_2017 <darijan.micic10@gmail.com>
2020-12-26 22:27:37 +0100

    #prikaz_studenata Pravljenje primerka kolekcije i kontrolera
    U radnji main klase App, pre pravljenja primerka klase MainFrame, napravljeni su primerci klasa StudentsCollection i StudentsController.

--- InformationSystem/src/application/App.java
+++ InformationSystem/src/application/App.java
@@ -1,13 +1,15 @@
 package application;
 
+import controller.StudentsController;
+import model.StudentsCollection;
 import view.MainFrame;
 
 public class App {
-	
 	public static void main(String[] args) {
 		/** REFERENCA: Materijali za vežbe (v5 -> a - Singleton -> Singleton.pdf) */
+		/** REFERENCA: Materijali za vežbe (v6 -> JTableMVCSimple -> igraci -> MainApp.java) */
+		StudentsCollection.getInstance();
+		StudentsController.getInstance();
 		MainFrame.getInstance();
-		
 	}
-
 }
