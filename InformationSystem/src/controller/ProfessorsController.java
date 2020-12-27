0710fed20e20c382dc9a14f1069648ee9cdeee87
Ognjen Srdic <srdic.ra46.2018@uns.ac.rs>
2020-12-27 16:17:31 +0100

    #profesor eliminisanje osisanih slova

--- InformationSystem/src/controller/ProfessorsController.java
+++ InformationSystem/src/controller/ProfessorsController.java
@@ -23,9 +23,9 @@ public class ProfessorsController {
 	private ProfessorsController() {}
 	
 	public void addProfessor() {
-		ProfessorsCollection.getInstance().addProfessor("Kovacevic","Miodrag",LocalDate.of(1982, 10, 10),
-				"Desanke Maksimovic 32, Kragujevac", "0613214457", "miodrag__kovacevic@yahoo.com",
-				"Vuka Karadzica 87, Beograd",1010982,TitleOfProfessor.DR,CallingOfProfessor.REDOVNI_PROFESOR);
+		ProfessorsCollection.getInstance().addProfessor("Kovačević","Miodrag",LocalDate.of(1982, 10, 10),
+				"Desanke Maksimović 32, Kragujevac", "0613214457", "miodrag__kovacevic@yahoo.com",
+				"Vuka Karadžića 87, Beograd",1010982,TitleOfProfessor.DR,CallingOfProfessor.REDOVNI_PROFESOR);
 		
 		MainFrame.getInstance().refreshView("ADDED", -1);
 	}
@@ -43,7 +43,7 @@ public class ProfessorsController {
 		if(rowSelectedIndex >= 0) {
 			Professor professor = ProfessorsCollection.getInstance().getRow(rowSelectedIndex);
 			ProfessorsCollection.getInstance().modifyProfessor("Ilijev", "Ivana", LocalDate.of(1989, 7, 8),
-					"Branislava Nusica 91, Beograd", "0639076833", "ivkailijev@gmail.com", 
+					"Branislava Nušića 91, Beograd", "0639076833", "ivkailijev@gmail.com", 
 					"Nikole Tesle 4, Beograd", 7089891, TitleOfProfessor.MR, CallingOfProfessor.ASISTENT);
 			
 			MainFrame.getInstance().refreshView(null, -1);
