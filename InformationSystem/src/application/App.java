e589a59955dc5cc48ec01e0cb6e2b4526fce4ee1
Ognjen Srdic <srdic.ra46.2018@uns.ac.rs>
2020-12-27 11:52:47 +0100

    #main sada poziva i ProfessorsCollection

--- InformationSystem/src/application/App.java
+++ InformationSystem/src/application/App.java
@@ -1,13 +1,18 @@
 package application;
 
+import controller.StudentsController;
+import model.ProfessorsCollection;
+import model.StudentsCollection;
 import view.MainFrame;
 
 public class App {
-	
 	public static void main(String[] args) {
 		/** REFERENCA: Materijali za vežbe (v5 -> a - Singleton -> Singleton.pdf) */
+		/** REFERENCA: Materijali za vežbe (v6 -> JTableMVCSimple -> igraci -> MainApp.java) */
+		StudentsCollection.getInstance();
+		StudentsController.getInstance();
+		ProfessorsCollection.getInstance();
 		MainFrame.getInstance();
 		
 	}
-
 }
