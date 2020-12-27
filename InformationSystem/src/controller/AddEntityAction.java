8d6ddfebb65edbd47ba55de4fdf7caab108a22a1
Darijan_Micic_RA_203_2017 <darijan.micic10@gmail.com>
2020-12-27 21:23:00 +0100

    #dodavanje_studenta Omogućeno prikazivanje dijaloga
    Napravljena je akcija AddEntityAction, koju je potrebno vezati za odgovarajuću stavku u meniju File i dugme u traci sa alatkama.

---
+++ InformationSystem/src/controller/AddEntityAction.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.KeyStroke;
+
+import view.MainFrame;
+import view.StudentAddingDialog;
+import view.StudentsTab;
+
+public class AddEntityAction extends AbstractAction {
+	public AddEntityAction() {
+		putValue(MNEMONIC_KEY, KeyEvent.VK_N);
+		putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_N, KeyEvent.CTRL_MASK));
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		Component selectedTab = MainFrame.getInstance().getTabbedPane().getSelectedComponent();
+		
+		if (selectedTab instanceof StudentsTab) {
+			StudentAddingDialog addingDialog = new StudentAddingDialog(MainFrame.getInstance());
+			addingDialog.setVisible(true);
+		}
+	}
+}
